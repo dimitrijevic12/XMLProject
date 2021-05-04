f762a68072e39cfdd0da1a541ef9a36832d13aaf
Nemanja Dimitrijevic <necapro@gmail.com>
2021-05-04 19:15:26 +0200

    fix: Code cleanup

--- nistagram-backend/Core/Core/UserMicroservice/RegisteredUser.cs
+++ nistagram-backend/Core/Core/UserMicroservice/RegisteredUser.cs
@@ -1,9 +1,6 @@
 ﻿using CSharpFunctionalExtensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.UserMicroservice
 {
