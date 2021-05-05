aebc888c990d5cfb126d3686c0c8dd79ee91f8d8
Nemanja Dimitrijevic <necapro@gmail.com>
2021-05-05 17:55:23 +0200

    fix: Add text to Comment

---
+++ nistagram-backend/Core/Core/PostMicroservice/CommentText.cs
@@ -0,0 +1,30 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+
+namespace Core.PostMicroservice
+{
+    public class CommentText : ValueObject
+    {
+        private readonly string text;
+
+        private CommentText(string text)
+        {
+            this.text = text;
+        }
+
+        public static Result<CommentText> Create(string text)
+        {
+            if (String.IsNullOrWhiteSpace(text)) return Result.Failure<CommentText>("Comment text cannot be empty");
+            if (text.Length > 50) return Result.Failure<CommentText>("Comment cannot contain more than 50 characters.");
+            return Result.Success(new CommentText(text));
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return text;
+        }
+
+        public static implicit operator string(CommentText commentText) => commentText.text;
+    }
+}
