f71462492cb7346f7954b99fd949bfa69c21255a
Nemanja Dimitrijevic <necapro@gmail.com>
2021-05-05 17:43:58 +0200

    feat: Implement Report microservice

--- nistagram-backend/Core/Core/UserMicroservice/PrivateFollowRequest.cs
+++ nistagram-backend/Core/Core/UserMicroservice/PrivateFollowRequest.cs
@@ -7,16 +7,16 @@ namespace Core.UserMicroservice
     {
         private readonly bool approved;
 
-        private PrivateFollowRequest(Guid id, DateTime timestamp, RegisteredUser requestsFollow, RegisteredUser recievesFollow, bool approved)
-            : base(id, timestamp, requestsFollow, recievesFollow)
+        private PrivateFollowRequest(Guid id, DateTime timestamp, RegisteredUser requestsFollow, RegisteredUser receivesFollow, bool approved)
+            : base(id, timestamp, requestsFollow, receivesFollow)
         {
             this.approved = approved;
         }
 
         public static Result<PrivateFollowRequest> Create(Guid id, DateTime timestamp, RegisteredUser requestsFollow,
-            RegisteredUser recievesFollow, bool approved)
+            RegisteredUser receivesFollow, bool approved)
         {
-            return Result.Success(new PrivateFollowRequest(id, timestamp, requestsFollow, recievesFollow, approved));
+            return Result.Success(new PrivateFollowRequest(id, timestamp, requestsFollow, receivesFollow, approved));
         }
     }
 }
