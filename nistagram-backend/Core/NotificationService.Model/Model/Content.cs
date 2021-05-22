9f3fc2d91ce034e2c16d316def8221a697220a59
Nemanja Dimitrijevic <necapro@gmail.com>
2021-05-22 14:12:01 +0200

    feat: Create notification microservice

---
+++ nistagram-backend/Core/NotificationService.Model/Model/Content.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NotificationService.Core.Model
+{
+    public abstract class Content
+    {
+        protected readonly Guid id;
+
+        protected Content(Guid id)
+        {
+            this.id = id;
+        }
+    }
+}
