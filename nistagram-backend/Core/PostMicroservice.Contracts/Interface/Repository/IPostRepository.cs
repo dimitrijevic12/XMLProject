dbe27086302a1fd150b394741734a7ec6a9243b0
Stefann11 <santrac.stefan@gmail.com>
2021-05-28 19:13:38 +0200

    feat: Post Repository

---
+++ nistagram-backend/Core/PostMicroservice.Contracts/Interface/Repository/IPostRepository.cs
@@ -0,0 +1,8 @@
+﻿using PostMicroservice.Core.Model;
+
+namespace PostMicroservice.Contracts.Interface.Repository
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+    }
+}
---
+++ nistagram-backend/Core/PostMicroservice.Contracts/Interface/Repository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace PostMicroservice.Contracts.Interface.Repository
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(int id);
+
+        T Save(T obj);
+
+        T Edit(T obj);
+
+        T Delete(T obj);
+    }
+}
