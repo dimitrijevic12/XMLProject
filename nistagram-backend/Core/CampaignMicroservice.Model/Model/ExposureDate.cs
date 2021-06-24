40e530c944b2722bdbbfd9402c8963030b055fa0
Nemanja Dimitrijevic <necapro@gmail.com>
2021-06-24 15:37:11 +0200

    feat: Bring over changes from docker branch

---
+++ nistagram-backend/Core/CampaignMicroservice.Model/Model/ExposureDate.cs
@@ -0,0 +1,32 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampaignMicroservice.Core.Model
+{
+    public class ExposureDate : ValueObject
+    {
+        public DateTime Time { get; }
+        public IEnumerable<RegisteredUser> SeenBy { get; }
+
+        private ExposureDate(DateTime time, IEnumerable<RegisteredUser> seenBy)
+        {
+            Time = time;
+            SeenBy = seenBy;
+        }
+
+        public Result<ExposureDate> Create(DateTime time, IEnumerable<RegisteredUser> seenBy)
+        {
+            return new ExposureDate(time, seenBy);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Time;
+            yield return SeenBy;
+        }
+    }
+}
--- nistagram-backend/Core/CampaignMicroservice.Model/Model/OneTimePostCampaign.cs
+++ nistagram-backend/Core/CampaignMicroservice.Model/Model/OneTimePostCampaign.cs
@@ -5,17 +5,17 @@ namespace CampaignMicroservice.Core.Model
 {
     public class OneTimePostCampaign : Campaign
     {
-        private readonly DateTime exposureDate;
+        private readonly ExposureDate exposureDate;
 
         private OneTimePostCampaign(Guid id, TargetAudience targetAudience, Agent agent,
-            CampaignStatistics campaignStatistics, DateTime exposureDate)
+            CampaignStatistics campaignStatistics, ExposureDate exposureDate)
             : base(id, targetAudience, agent, campaignStatistics)
         {
             this.exposureDate = exposureDate;
         }
 
         public static Result<OneTimePostCampaign> Create(Guid id, TargetAudience targetAudience, Agent agent,
-            CampaignStatistics campaignStatistics, DateTime exposureDate)
+            CampaignStatistics campaignStatistics, ExposureDate exposureDate)
         {
             return Result.Success(new OneTimePostCampaign(id, targetAudience, agent,
             campaignStatistics, exposureDate));
