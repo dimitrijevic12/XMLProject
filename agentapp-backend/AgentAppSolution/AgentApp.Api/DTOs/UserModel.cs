ca00e0517e8f9b3d5f1c2f2e9ed7d2a292c9657c
Aleksa Santrač <akimbo.santrac@gmail.com>
2021-06-27 17:45:34 +0200

    feat: agent registration and login backend

---
+++ agentapp-backend/AgentAppSolution/AgentApp.Api/DTOs/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgentApp.Api.DTOs
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public String Role { get; set; }
+    }
+}
--- agentapp-backend/AgentAppSolution/AgentApp.Api/Startup.cs
+++ agentapp-backend/AgentAppSolution/AgentApp.Api/Startup.cs
@@ -1,16 +1,16 @@
+using AgentApp.Core.Interface.Repository;
+using AgentApp.Core.Services;
+using AgentApp.DataAccess.Implementation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Text;
 
 namespace AgentApp.Api
 {
@@ -38,6 +38,27 @@ namespace AgentApp.Api
                        .AllowAnyMethod()
                        .AllowAnyHeader();
             }));
+
+            services.AddScoped<UserService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                };
+            });
+            services.AddControllersWithViews();
+            /* .AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);*/
+            services.AddAuthorization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +74,7 @@ namespace AgentApp.Api
 
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseRouting();
 
             app.UseAuthorization();
