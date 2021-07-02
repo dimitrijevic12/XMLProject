c1d3b37e4dab3ce6b6a0826e76215845b100381d
Stefann11 <santrac.stefan@gmail.com>
2021-07-02 18:47:08 +0200

    feat: campaign requests

---
+++ nistagram-backend/Core/CampaignMicroservice.Api/Controllers/CampaignRequestsController.cs
@@ -0,0 +1,22 @@
+﻿using CampaignMicroservice.Core.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampaignMicroservice.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampaignRequestsController : ControllerBase
+    {
+        private readonly ICampaignRequestRepository _campaignRequestRepository;
+
+        public CampaignRequestsController(ICampaignRequestRepository campaignRequestRepository)
+        {
+            _campaignRequestRepository = campaignRequestRepository;
+        }
+    }
+}
